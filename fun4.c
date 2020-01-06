1abb001c9076c20acace3d66523b259b8bc85673
camilooob <camilobaq@hotmail.com>
2020-01-05 23:42:08 -0500

    betty ok and flags

---
+++ fun4.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+global_t global;
+
+/**
+ * get_func - opcode
+ * @opcode: output
+ * Return: Nothing.
+ */
+int get_func(char *opcode)
+{
+	instruction_t fun[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{NULL, NULL}
+
+	};
+
+	int i;
+
+	i = 0;
+	for (i = 0; fun[i].opcode && opcode; i++)
+	{
+		if ((strcmp(fun[i].opcode, opcode) == 0))
+		{
+			fun[i].f(&global.headstack, global.line_number);
+			return (0);
+		}
+	}
+	return (0);
+}
